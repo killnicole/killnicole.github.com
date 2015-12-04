1bec103d89fa7f276e0fdc0cc2775566203dea7f
Rafal Bromirski <rafal.bromirski@gmail.com>
2015-12-05 00:12:00 +0100

    more more more!

--- src/js/application.js
+++ src/js/application.js
@@ -1,1 +1,11 @@
-console.log('hello!');
+var $ = require('jquery');
+
+$('.js-scroll-along').on('click', function(e) {
+  e.preventDefault();
+
+  var target = $(this).attr('href');
+
+  $('html, body').animate({
+    scrollTop: $(target).offset().top
+  }, 500);
+})
